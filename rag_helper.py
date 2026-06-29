79bcf1c335dec7d1af88908aebbc9997bcae6e46
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2026-06-29 14:48:26 +0000

    02-vector-search

--- rag_helper.py
+++ rag_helper.py
@@ -81,3 +81,52 @@ class RAGBase:
         prompt = self.build_prompt(query, search_results)
         answer = self.llm(prompt)
         return answer
+    
+class RAGVector(RAGBase):
+
+    def __init__(self, embedder, **kwargs):
+        super().__init__(**kwargs)
+        self.embedder = embedder
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        filter_dict = {"course": self.course}
+
+        vec_index = self.index.search(
+            query_vector,
+            num_results=num_results,
+            filter_dict=filter_dict
+        )
+
+        self.index.close()
+        return vec_index
+
+class RAGPgVector(RAGBase):
+
+    def __init__(self, embedder, conn, **kwargs):
+        super().__init__(index=None, **kwargs)
+        self.embedder = embedder
+        self.conn = conn
+
+    def vec_to_str(self, vector):
+        return "[" + ",".join(str(x) for x in vector) + "]"
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        query_str = self.vec_to_str(query_vector)
+
+        rows = self.conn.execute(
+            """
+            SELECT course, section, question, answer
+            FROM documents
+            WHERE course = %s
+            ORDER BY embedding <=> %s::vector
+            LIMIT %s
+            """,
+            (self.course, query_str, num_results)
+        ).fetchall()
+
+        return [
+            {"course": r[0], "section": r[1], "question": r[2], "answer": r[3]}
+            for r in rows
+        ]
